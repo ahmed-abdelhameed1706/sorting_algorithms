2e24577106f54b600592823762a420ddc1aa1f0a
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-07-23 16:28:34 -0700

    task-3

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array with selection algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Return: nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, smallest;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		smallest = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[smallest])
+			{
+				smallest = j;
+			}
+		}
+
+		temp = array[i];
+		array[i] = array[smallest];
+		array[smallest] = temp;
+		print_array(array, size);
+	}
+}
