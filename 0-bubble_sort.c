0bb62743262e6e3b66d6afc5d39f4fe3c735d47b
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-07-23 13:21:54 -0700

    task 1

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function to bubble sort an array
+ * @array: array of ints to be sorted
+ * @size: size of the array
+ *
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp, sorted_flag = 0;
+	size_t i;
+
+	while (sorted_flag == 0)
+	{
+		sorted_flag = 1;
+
+		for (i = 0; i < size - 1; i++)
+		{			
+			if (array[i] > array[i + 1])
+			{
+				sorted_flag = 0;
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+ this is the sorting_algorithms project 
